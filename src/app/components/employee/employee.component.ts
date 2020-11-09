15642bf75ecbba4396b14578c227c84746a4aed9
leandro <carolino.leandro@gmail.com>
2020-11-09 15:03:25 +0100

    initial commit

---
+++ src/app/components/employee/employee.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+  firstName: string = 'Leandro';
+  lastName: string = 'Santos';
+  gender: string = 'Male';
+  age: number = 20;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
